ef2672ff1c39a88975e2406aebf167d9af2634cd
Ben Yu <intenex@gmail.com>
2019-02-18 05:17:24 -0800

    Finished making the stack.rb

---
+++ W1D5/stack.rb
@@ -0,0 +1,17 @@
+class Stack # LIFO # damn works so well love it so cool man
+  def initialize
+    @stacks = Array.new
+  end
+
+  def push(el)
+    @stacks << el
+  end
+
+  def pop
+    @stacks.pop
+  end
+
+  def peek
+    @stacks[-1]
+  end
+end
