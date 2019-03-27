4bf926a665a6df8227ea80b46029583053afdf71
Ben Yu <intenex@gmail.com>
2019-03-27 00:02:11 -0700

    Finish models

--- W3D3/homes/app/models/house.rb
+++ W3D3/homes/app/models/house.rb
@@ -1,9 +1,25 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id      :bigint(8)        not null, primary key
+#  address :string
+#
+
 class House < ApplicationRecord
-    
+    validates :address, presence: true, uniqueness: true
+
     has_many(
-        :person,
+        :residents,
         class_name: 'Person',
         foreign_key: :house_id,
         primary_key: :id
     )
+
+    before_save :downcase_address # runs this after initialization but before saving to the databsae
+
+    private
+    def downcase_address # the method name referenced in before_save
+        address.downcase!
+    end
 end
--- W3D3/homes/app/models/person.rb
+++ W3D3/homes/app/models/person.rb
@@ -1,8 +1,27 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id       :bigint(8)        not null, primary key
+#  name     :string
+#  house_id :integer
+#
+
 class Person < ApplicationRecord
+    validates :name, presence: true
+
     belongs_to(
         :house,
         class_name: 'House',
         foreign_key: :house_id,
         primary_key: :id
     )
+
+    before_validation :dummy_method
+    before_update :dummy_method
+    before_create :dummy_method
+    before_save :dummy_method
+    after_save :dummy_method
+
+    def dummy_method; end
 end
