2aaa72048a8ae8131f940f25f1bdeb66a9c8f2c1
Ben Yu <intenex@gmail.com>
2019-04-11 21:26:35 -0700

    Some comments

--- W5D5/readline.js
+++ W5D5/readline.js
@@ -3,8 +3,8 @@ const readline = require('readline'); // require the 'readline' library which re
 // then call with our readline constant love it lol
 
 const reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input: process.stdin, // this specifies the input channel for getting a response back from reader.question and I'm sure other methods
+    output: process.stdout // this specifies the output channel to print out the first argument too in reader.question and I'm sure other methods love it so great and fortunate to actually understand everything can't wait to keep pushing and getting everything 
 }); // as an assignment you need semicolons love it but JS is so nice it lets you
 // run things even with bad syntax lmao so nuts
 
