e276d18edd9feb023e797b33ebf6d3f35869c8e7
Ben Yu <intenex@gmail.com>
2019-04-11 21:01:52 -0700

    Finish tea and biscuits

---
+++ W5D5/readline.js
@@ -0,0 +1,38 @@
+const readline = require('readline'); // require the 'readline' library which returns as its
+// `module.export` return evaluation some object with a bunch of methods set as attributes on it that we can
+// then call with our readline constant love it lol
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}); // as an assignment you need semicolons love it but JS is so nice it lets you
+// run things even with bad syntax lmao so nuts
+
+// So lucky to have Mai and to get to talk to her every day so great
+
+function teaAndBiscuits() {
+    let tea = "don't";
+    let biscuits = "don't";
+
+    function askForTea(){
+        reader.question("Would you like tea? Y/N\n", parseTeaAndAskForBiscuits)
+    }
+
+    function parseTeaAndAskForBiscuits(teaAnswer){
+        if (teaAnswer.toUpperCase() === "Y") {tea = "do";} // right semicolon on the inside love it
+        console.log(`So you ${tea} want tea!`)
+        reader.question("Would you like biscuits? Y/N\n", parseTeaAndBiscuits)
+    }
+
+    function parseTeaAndBiscuits(biscuitAnswer){
+        if (biscuitAnswer.toUpperCase() === "Y") {biscuits = "do";}
+        console.log(`So you ${biscuits} want biscuits!`)
+        console.log(`So you ${tea} want tea and you ${biscuits} want biscuits!`)
+        reader.close();
+    }
+
+    askForTea();
+}
+
+teaAndBiscuits(); // fucking amazing so proud of yourself totally get it lucky
+// lucky to really get this stuff
