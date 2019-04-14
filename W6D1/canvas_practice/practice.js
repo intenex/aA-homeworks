c1289a5de792a2848f577b73c2e78d76215b6245
Ben Yu <intenex@gmail.com>
2019-04-14 01:46:24 -0700

    Made rectangle

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+    const canvas = document.getElementById('mycanvas');
+    canvas.style.width = "500px";
+    canvas.style.height = "500px";
+    const ctx = canvas.getContext('2d'); // sets a basic Canvas 2d rendering context as per https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+    ctx.fillStyle = "#2372C1";
+    ctx.fillRect(100, 100, 200, 200); // amazing so lucky to get to try all this out
 });
