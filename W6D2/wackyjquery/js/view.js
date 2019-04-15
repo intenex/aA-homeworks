585881bea492d98b10c476bf5ba642b374400128
Ben Yu <intenex@gmail.com>
2019-04-15 12:53:14 -0700

    Finished homework

--- W6D2/wackyjquery/js/view.js
+++ W6D2/wackyjquery/js/view.js
@@ -23,6 +23,7 @@ View.prototype.exercise1 = function () {
   //Result: Every square should turn orange (we already have a CSS rule)
 
   //your code here!
+  $('li').addClass("orange");
 };
 
 View.prototype.exercise2 = function () {
@@ -30,6 +31,7 @@ View.prototype.exercise2 = function () {
   //Result: Every square vanishes
 
   //your code here!
+  $('li').remove(); // hmm why did they do the parent thing lol this works just straight love it
 };
 
 View.prototype.exercise3 = function () {
@@ -37,6 +39,9 @@ View.prototype.exercise3 = function () {
   //Result: An <h1> with the text 'i love jquery' appears under the grid.
 
   //your code here!
+  let $header = $("<h1 style='text-align: center'>i love jquery</h1>"); // creates a jQuery object of an HTMLElement that has that HTML so amazing good to have learned it so well at least that's key you didn't learn that much but the stuff you did you learned well which is very lucky keep pushing on the quiz for sure this is better for the long run not the tests for sure though though that stuff is great too so def get into it too
+  // $('body').append("<h1 style='text-align: center'>i love jquery</h1>")
+  $('body').append($header);
 };
 
 View.prototype.exercise4 = function () {
@@ -44,17 +49,34 @@ View.prototype.exercise4 = function () {
   //Result: Your name appears in every other square.
 
   //your code here!
+  $('li:odd').text("ben"); // jfc jQuery is just SO easy lol it makes everything so crazy easy to do
 };
 
 View.prototype.exercise5 = function () {
   //Challenge: Alert the row and column of the square, when the square is clicked.
   //Result: When a square is clicked, the row and column appear in an alert. for
   //example: clicking the top left square should alert '0, 0'.
-
+ 
   //hint: checkout the addRow function at the bottom of the file: we set the
   //  'data-pos' of every square
 
   //your code here!
+  let $allLis = $('li'); // get all the lis and data-pos to them and then alert it
+  let allLis = Array.from($allLis);
+  allLis.forEach(function(el, idx) { // ah don't think there's a forEach method on allLis rn lol
+    let x = idx;
+    let y = 0;
+    if (idx >= 20) {
+      x = idx % 20;
+      y = Math.floor(idx/20);
+    }
+    let $el = $(el);
+    $el.data('pos', [x, y]); // holy fuck it works lmao
+  });
+  $allLis.on("mouseover", () => {
+    let $currentLi = $(event.currentTarget); // right must wrap it only jQuery objects have the .data and .on and all that fucking love it
+    alert($currentLi.data('pos')); // love it 
+  });
 };
 
 View.prototype.exercise6 = function () {
@@ -65,6 +87,9 @@ View.prototype.exercise6 = function () {
   //hint: use window._randomColorString() (defined at top) to get a random color!
 
   //your code here!
+  $('li').each((idx, el) => { // the function first passes the index then the element which is an HTMLElement (an HTMLLiElement to be exact amazing lol)
+    $(el).css('background-color', window._randomColorString()); // fucking amazing really getting the hang of jQuery which is the coolest fucking shit ever you really can do everything in the world must remember to wrap it up in the $(el) to make it a jQuery object to be able to call .css and shit on it so amazing
+  });
 };
 
 View.prototype.exercise7 = function(){
@@ -75,6 +100,9 @@ View.prototype.exercise7 = function(){
   //rainbow.
 
   //your code here!
+  $('li').on("mouseover", () => {
+    console.log($(event.currentTarget).css('background-color')); // fucking love it exercise 5 was the hardest by far lmao but you fucking did it
+  });
 };
 
 
